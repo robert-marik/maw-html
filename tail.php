130d2d21f8c7ce0ad93b97411e7f9852fa9c74b9
Robert Marik <marik@mendelu.cz>
2010-04-19 10:21:53 +0200

    The tail of the page (with counters and load diagram) moved to tail_user.php file which is site specific.

--- tail.php
+++ tail.php
@@ -3,60 +3,10 @@
 <br>
 The project Mathematical Assistant on Web is hosted on <a
 href="http://mathassistant.sourceforge.net/">sourceforge.net</a> and supported by Grant 99/2008 of <a href="http://www.frvs.cz/">FRVŠ</a>.
-<br>
-<br>
-<?php echo __("Server load in last hour: "); ?><a href="http://wood.mendelu.cz/math/HotSaNIC/system/load.html"><img
-src="http://wood.mendelu.cz/math/load.png"></a>
-<br>
-<br>
 
-<!-- Start of StatCounter Code -->
-<script type="text/javascript">
-var sc_project=3308553; 
-var sc_invisible=0; 
-var sc_partition=36; 
-var sc_security="d038c23d"; 
-</script>
-
-<script type="text/javascript"
-src="http://www.statcounter.com/counter/counter_xhtml.js"></script><noscript><div 
-class="statcounter"><a class="statcounter"
-href="http://www.statcounter.com/"><img class="statcounter"
-src="http://c37.statcounter.com/3308553/0/d038c23d/0/" alt="blog stats"
-/></a></div></noscript>
-<!-- End of StatCounter Code --><br><a
-href="http://my.statcounter.com/project/standard/stats.php?project_id=3308553&guest=1">View 
-My Stats</a>
-
-<br> <br>
-<!-- Start of Toplist Code -->
-<a href="http://www.toplist.cz/stat/726662"><script language="JavaScript" type="text/javascript">
-<!--
-document.write ('<img src="http://toplist.cz/count.asp?id=726662&logo=s&http='+escape(document.referrer)+'&wi='+escape(window.screen.width)+'&he='+escape(window.screen.height)+'" width="14" height="14" border=0 alt="TOPlist" />');
-//--></script><noscript><img src="http://toplist.cz/count.asp?id=726662&logo=s" border="0"
-alt="TOPlist" width="14" height="14" /></noscript></a>
-<!-- End of Toplist Code -->
-
-<a href="http://statistiky.monitoring-serveru.cz/maw"><img src="http://data.monitoring-serveru.cz/maw/ikonka-modroseda.png" width="100" height="25" alt="Accessibility" style="border-width: 0px;" /></a> 
-<a href="http://statistiky.monitoring-serveru.cz/1234628"><img src="http://data.monitoring-serveru.cz/1234628/ikonka-tyrkysova.png" width="100" height="25" alt="Dostupnost podle Monitoring-servercz" border="0"></a>
-<a href="http://statistiky.monitoring-serveru.cz/maw">
-<?php echo __("uptime");?></a>
-<br>
-  <!-- Histats.com  START  --> 
- <a href="http://www.histats.com" target="_blank" title="free hit counter" > 
- <script  type="text/javascript" language="javascript"> 
-  
- var s_sid = 305361;var st_dominio = 4; 
- var cimg = 603;var cwi =110;var che =40; 
- </script> 
- <script  type="text/javascript" language="javascript" src="http://s10.histats.com/js9.js"></script> 
- </a> 
- <noscript><a href="http://www.histats.com" target="_blank"> 
- <img  src="http://s4.histats.com/stats/0.gif?305361&1" alt="free hit counter" border="0"></a> 
- </noscript> 
-  
- <!-- Histats.com  END  --> 
-
+<?php
+if (file_exists('./tail_user.php')) {require ('./tail_user.php');}
+?>
 
 
 </body>
