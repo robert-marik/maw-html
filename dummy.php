0f63637d109e20eb7c65f00fea4d350a34760cae
Robert Marik <marik@mendelu.cz>
2012-02-09 12:54:15 +0100

    Updated linenumbers, po files will be filtered through msgcat before comitting.

---
+++ dummy.php
@@ -0,0 +1,11 @@
+dummy messages for MAW (maw-html part)
+Installed on the main site, not in the sources
+
+<?php
+
+__("Switch to a compact design")
+__("Showcase")
+__("Switch to a full design")
+__("Did we help you with your math problem? Did we save your time? If so, consider to send some money to <a href=\"http://www.unicef.org/\">UNICEF foundation.</a>")
+
+?>
